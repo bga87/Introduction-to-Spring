75cdea4eab4fae54d06db2ddd93bfbf473dc5b73
Gleb Bespalov <ga.bespalov@gmail.com>
2020-09-03 14:04:49 +1100

    Метод main реализован в соответствии с условием задачи

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
         System.out.println(bean.getMessage());
+
+        System.out.println("bean == bean2: " + (bean == bean2));
+        System.out.println("cat == cat2: " + (cat == cat2));
     }
 }
